15f87efc195497c3fe3d66b4c9c39e7bb069a6fd
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-27 00:13:18 +0500

    Complete 'coin_change' function

--- decision/core/partition.py
+++ decision/core/partition.py
@@ -32,38 +32,36 @@ def coin_change(amount: int,
         return (_two_coin_change(amount, denominations[0], denominations[1])
                 + zeros(len(denominations) - 2))
     else:
-        last_denomination_index = denominations_count - 1
-        last_denomination = denominations[last_denomination_index]
-        extra = None
-        if amount <= denominations[-1]:
-            ceil_index = bisect_left(denominations, amount)
-            ceil_denomination = denominations[ceil_index]
-            if amount == ceil_denomination:
-                return (zeros(ceil_index) + (1,)
-                        + zeros(len(denominations) - ceil_index - 1))
-            extra = (zeros(ceil_index)
-                     + (ceil_division(amount, ceil_denomination),)
-                     + zeros(len(denominations) - ceil_index - 1))
-
         def key(counts: Tuple[int, ...]) -> Tuple[int, int]:
             return (sum(count * denomination
                         for count, denomination in zip(counts, denominations)
                         if count),
                     sum(counts))
 
+        if amount <= denominations[-1]:
+            ceil_index = bisect_left(denominations, amount)
+            ceil_denomination = denominations[ceil_index]
+            zeros_tail = zeros(len(denominations) - ceil_index - 1)
+            return (zeros(ceil_index) + (1,) + zeros_tail
+                    if amount == ceil_denomination
+                    else min(zeros(ceil_index)
+                             + (ceil_division(amount, ceil_denomination),),
+                             coin_change(amount, denominations[:ceil_index],
+                                         ceil_index) + (0,),
+                             key=key) + zeros_tail)
+        last_denomination_index = denominations_count - 1
+        last_denomination = denominations[last_denomination_index]
         max_last_denomination_count = amount // last_denomination
-        result = min([_add_at_index(coin_change(start_amount, denominations,
-                                                last_denomination_index),
-                                    last_denomination_index,
-                                    last_denomination_count)
-                      for last_denomination_count, start_amount
-                      in zip(range(max_last_denomination_count + 1),
-                             accumulate(chain((amount,),
-                                              repeat(last_denomination)),
-                                        sub))],
-                     key=key)
-        return result if extra is None else min(result, extra,
-                                                key=key)
+        return min([_add_at_index(coin_change(start_amount, denominations,
+                                              last_denomination_index),
+                                  last_denomination_index,
+                                  last_denomination_count)
+                    for last_denomination_count, start_amount
+                    in zip(range(max_last_denomination_count + 1),
+                           accumulate(chain((amount,),
+                                            repeat(last_denomination)),
+                                      sub))],
+                   key=key)
 
 
 def coin_changes(amount: int,
