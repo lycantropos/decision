efaaee85b2c464c2853af581221515c0ca3284c6
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-03 13:52:47 +0300

    Complete 'coins_counters' function

--- decision/core/partition.py
+++ decision/core/partition.py
@@ -83,8 +83,8 @@ def coins_counters(amount: int,
     else:
         last_denomination_index = denominations_count - 1
         last_denomination = denominations[last_denomination_index]
-        max_last_denomination_count, remainder = divmod(amount,
-                                                        last_denomination)
+        max_last_denomination_count, amount_remainder = divmod(
+                amount, last_denomination)
         start_amount = amount
         for last_denomination_count in range(max_last_denomination_count + 1):
             for candidate in coins_counters(start_amount, denominations,
@@ -92,7 +92,7 @@ def coins_counters(amount: int,
                 yield _add_at_index(candidate, last_denomination_index,
                                     last_denomination_count)
             start_amount -= last_denomination
-        if remainder:
+        if amount_remainder:
             yield (_zeros(last_denomination_index)
                    + (1
                       if amount < last_denomination
